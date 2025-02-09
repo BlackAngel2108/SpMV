aacfa687396f6059f2bfad31dbbf703a5251ccc5
BlackAngel2108 <luba.sobol.nn@mail.ru>
2025-02-10 00:46:45 +0300

    cmake

--- matrix_binary_library/matrix_to_bin.cpp
+++ matrix_binary_library/matrix_to_bin.cpp
@@ -98,7 +98,7 @@ std::vector<MatrixElement> readMatrixFromBinaryCOO(const std::string& filename, 
 }
 
 int main() {
-    std::string matrix_folder = "../../matrix_to_git";
+    std::string matrix_folder = "../../matrix";
     std::string bin_folder = "../../bin_matrix";
 
     if (!fs::exists(matrix_folder) || !fs::is_directory(matrix_folder)) {
